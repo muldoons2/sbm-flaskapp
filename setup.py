07ffccc08344a8564763bf2135dff12149eb48a3
muldoons2 <59346412+muldoons2@users.noreply.github.com>
2020-05-03 21:28:42 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+   name='sbm-flaskapp',
+   version='1.0',
+   description='A useful module',
+   author='Sean Muldoon',
+   packages=['sbm-flaskapp'],  #same as name
+)
