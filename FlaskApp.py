adc2f4d386a26a97d058733ddcb644737a2745ae
muldoons2 <59346412+muldoons2@users.noreply.github.com>
2020-05-03 21:30:21 -0400

    Update FlaskApp.py

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,28 +1,10 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[ ]:
-
-
 from flask import Flask, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
 
-############################################################
-#### This part is related to creating a Database, which is not complete
-#### Video instructions are here: https://www.youtube.com/watch?v=Z1RJmh_OqeA
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-# db = SQLAlchemy(app)
-# class todo(db.model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     content = db.Column(db.String(200), nullable=False)
-#     completed = db.Column(db.Integer, default=0)
-#     date_created = db.Column(db.DateTime, default = datetime.utcnow)
-# def __repr__(self):
-#     return '<task %r>' % self.id
-############################################################
-    
 @app.route("/")
 def home(): return render_template('home.html')
 
@@ -37,23 +19,3 @@ def hello_sean(): return ("Hello, Sean!")
 
 if __name__ == "__main__":
     app.run(host='localhost',port=5000)
-#     app.run(host='localhost',port=5000,debug=True)
-
-
-# In[1]:
-
-
-#cd /Users/seanmuldoon/Desktop/Python_Mac/FlaskIntro
-
-# pip install virtualenv
-# virtualenv env
-# source env/bin/activate
-
-# !pip install flask_sqlalchemy
-
-
-# In[ ]:
-
-
-
-
