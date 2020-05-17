db45437d23c272c464fd641fdce706a73aa0c011
muldoons2 <59346412+muldoons2@users.noreply.github.com>
2020-05-17 15:21:59 -0400

    Update FlaskApp.py

--- FlaskApp.py
+++ FlaskApp.py
@@ -16,8 +16,8 @@ app = Flask(__name__)
 #     return '<task %r>' % self.id
 ############################################################
     
-@app.route("/")
-def home(): return render_template('home.html')
+# @app.route("/")
+# def home(): return render_template('home.html')
 
 @app.route("/index")
 def index(): return render_template('index.html')
