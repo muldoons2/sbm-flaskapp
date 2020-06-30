74c5b1ffdd3d32d2ed9b1e3699dabd316d8e08e6
muldoons2 <59346412+muldoons2@users.noreply.github.com>
2020-06-30 01:15:09 -0400

    Update FlaskApp.py

--- FlaskApp.py
+++ FlaskApp.py
@@ -28,6 +28,7 @@ def base(): return render_template('base.html')
 @app.route("/hello_sean")
 def hello_sean(): return ("Hello, Sean!")
 
-if __name__ == "__main__":
-    app.run(host='localhost',port=5000)
-#     app.run(host='localhost',port=5000,debug=True)
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
