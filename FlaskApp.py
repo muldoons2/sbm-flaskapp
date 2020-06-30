6b1a8af377ba260ccb425c3589444be488328ace
muldoons2 <59346412+muldoons2@users.noreply.github.com>
2020-06-30 02:02:19 -0400

    Update FlaskApp.py

--- FlaskApp.py
+++ FlaskApp.py
@@ -1,36 +1,33 @@
-#!/usr/bin/env python3
 import os
-from flask import Flask, render_template, request
-import pandas as pd
+from flask import Flask, render_template, url_for
+from datetime import datetime
 app = Flask(__name__)
 
-x = []
-
+############################################################
+#### This part is related to creating a Database, which is not complete
+#### Video instructions are here: https://www.youtube.com/watch?v=Z1RJmh_OqeA
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+# db = SQLAlchemy(app)
+# class todo(db.model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     content = db.Column(db.String(200), nullable=False)
+#     completed = db.Column(db.Integer, default=0)
+#     date_created = db.Column(db.DateTime, default = datetime.utcnow)
+# def __repr__(self):
+#     return '<task %r>' % self.id
+############################################################
+    
 @app.route("/")
-def home(): return render_template('home.html')
+def home(): return render_template('templates/home.html')
 
 @app.route("/index")
-def index(): return render_template('index.html')
+def index(): return render_template('templates/index.html')
 
 @app.route("/base")
-def base(): return render_template('base.html')
+def base(): return render_template('templates/base.html')
 
 @app.route("/hello_sean")
 def hello_sean(): return ("Hello, Sean!")
-
-# @app.route("/register", methods=["POST"])
-# def register():
-#     fname = request.form.get("fname")
-#     lname = request.form.get("lname")
-#     y = [{fname},{lname}]
-#     x.append(y)
-#     return (x)
-
-@app.route("/success")
-def success(): 
-    fname = request.args.get("fname")
-    lname = request.args.get("lname")
-    return render_template('success.html', fname=fname, lname=lname)
 
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
